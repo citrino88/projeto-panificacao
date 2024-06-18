6acce489a699a55bb84e1e110c7d4c7d836672f7
Bernardodass <bernardo113322@gmail.com>
2024-06-18 14:42:34 -0300

    ...

---
+++ cabecalho.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panificação</title>
+</head>
+<body>
+    
+
+</head>
+<body>
+    <header>
+        <div class="cabecalho" >
+            <h1><a href="index.html">Cake</a></h1>
+            <nav>
+                <h2><a href="">Menu</a></h2>
+                <ul class="links-menu" >
+                    <li><a href="index.html">Início</a></li>
+                    <li><a href="produtos.html"></a>Produtos</li>
+                    <li><a href="sobre.html">Sobre</a></li>
+                    <li><a href="contato.html">Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+
+</body>
+</html>
