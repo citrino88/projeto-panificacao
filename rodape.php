2fb2a5a4f4da04f48c6fda729818b932af926abf
Mmarli18 <161735439+Mmarli18@users.noreply.github.com>
2024-06-20 17:29:07 -0300

    Add files via upload

---
+++ rodape.php
@@ -0,0 +1,41 @@
+</div>
+</main>
+<footer class="footer mt-auto py-3 bg border-top">
+
+<div class="container">
+<section class="fundo">
+<div class="d-flex">  
+  <div class="p-2 flex-fill">
+    <img src="img/logo.webp" alt="Logo Cake" width="100" height="100"></div>
+  <div class="p-2 flex-fill ">
+  <div class="info" style="width: max-content;"> 
+  <h4>Entre em contato</h4>
+                <p>Telefone: <a href="tel:+551195555555">(11) 2135-0300</a></p>
+                <p>E-mail: <a href="mailto:contato@meuemail.com">contato@panificacao.com</a></p>
+                <p>Endereço: Rua Francisco Coimbra, 403</p></div>
+                </div> 
+
+  <div class="p-2 flex-fill">
+    <p>Redes Sociais</p>
+    <div class="icones" style="width:max-content">
+<a href="https://www.instagram.com/" target="_blank"><img src="img/instagram.png" alt="Icone instagram"></a>
+             <a href="https://www.facebook.com/" target="_blank"><img src="img/facebook.png" alt="Icone instagram"></a>
+               <a href="https://x.com/?lang=pt-br"target="_blank"><img src="img/twiter.png" alt="Icone instagram"></a></div>
+
+</section>
+        
+        <hr>
+        <p class="rodape-info text-center">
+            &copy; Todos os direitos reservados Grupo cake
+    
+        Panificação e Bolos é um site fictício desenvolvido para fins didáticos | Senac Penha &copy; 2024
+</p>
+   
+
+</footer>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
