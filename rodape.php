0a2f8a9deca091c1573264cd0a477ca6861648b4
Mmarli18 <161735439+Mmarli18@users.noreply.github.com>
2024-06-26 14:20:23 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -1,12 +1,61 @@
 </div>
 </main>
+<style>
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    padding: 30px;
+    background-color: #cdc2ae;
+    justify-content: center;
+    align-items: center;
+  }
+  .logo,
+  .contato,
+  .redes-sociais {
+    flex: auto;
+    padding: 10px;
+  }
 
-<footer class="footer mt-auto py-3 bg-light border-top">
-    <div class="container text-center">
-        Panificação e Bolos é um site fictício desenvolvido para fins didáticos | Senac Penha &copy; 2024
+  .contato h2,
+  .redes-sociais h2,
+  .copy h2 {
+    margin-bottom: 10px;
+  }
+
+  .copy {
+    text-align: center;
+  }
+</style>
+<footer class="footer">
+  <div class="logo">
+    <img src="img/logo.webp" alt="Logo Cake" width="100" height="100">
+  </div>
+  <div class="contato">
+    <h2>Entre em contato</h2>
+    <p>Telefone: <a href="tel:+551195555555">(11) 2135-0300</a></p>
+    <p>E-mail: <a href="mailto:contato@meuemail.com">contato@panificacao.com</a></p>
+    <p>Endereço: Rua Francisco Coimbra, 403</p>
+  </div>
+  <div class="redes-sociais">
+    <h2>Redes Sociais</h2>
+    <div class="icones" style="width:max-content">
+      <a href="https://www.instagram.com/" target="_blank"><img src="img/instagram.png" alt="Icone instagram"></a>
+      <a href="https://www.facebook.com/" target="_blank"><img src="img/facebook.png" alt="Icone instagram"></a>
+      <a href="https://x.com/?lang=pt-br" target="_blank"><img src="img/twiter.png" alt="Icone instagram"></a>
     </div>
+  </div>
+  <hr>
+  </div>
+
 </footer>
 
+<div class="copy">
+  <p>&copy; Todos os direitos reservados Grupo cake
+
+    Panificação e Bolos é um site fictício desenvolvido para fins didáticos | Senac Penha &copy; 2024
+  </p>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
